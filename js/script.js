1bc1fbad0a8154752d8d07d1e47b55e613d0fb87
Zhai90 <zhaihongmeng@gmail.com>
2021-09-12 19:40:17 -0300

    Adding files...

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var tooltipTriggerList = [].slice.call($('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+$(window).on('load', function() {
+  $('#welcomemodal').modal('show');
+});
