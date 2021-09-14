228bf705232624d943138ba4af9daf268b1e1bfa
Zhai90 <zhaihongmeng@gmail.com>
2021-09-14 14:55:07 -0300

    Updated credits section, added an embed stop script that doesn't even work for some reason

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+////////////////////////////////////////
+//   1 Javascript File 2 HTML files   //
+////////////////////////////////////////
+
 var tooltipTriggerList = [].slice.call($('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
@@ -6,3 +10,11 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 $(window).on('load', function() {
   $('#welcomemodal').modal('show');
 });
+
+document.getElementById("modalstop").addEventListener("click",    function(evt){
+  var video = document.getElementsByClassName("ytvideo");
+  for (var i=0; i<video.length; i++) {
+     video.item(i).contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+  }
+  
+  });
